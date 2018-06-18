83d5ef5024a94ab5073dd28ee0ce8e6dfec9d081
MarcusJones <jones.0bj3@gmail.com>
2018-06-18 15:28:45 +0200

    add Basic LED hardware test

---
+++ mule/parts/led.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+import logging
+
+PIN_BLUE1   = 26
+PIN_BLUE2   = 19
+PIN_BLUE3   = 13 
+PIN_BLUE4   = 6
+
+PIN_RED     = 16
+PIN_YELLOW  = 20
+PIN_GREED   = 21
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(PIN_BLUE1,GPIO.OUT)
+GPIO.setup(PIN_BLUE2,GPIO.OUT)
+GPIO.setup(PIN_BLUE3,GPIO.OUT)
+GPIO.setup(PIN_BLUE4,GPIO.OUT)
+
+GPIO.setup(PIN_RED,GPIO.OUT)
+GPIO.setup(PIN_YELLOW,GPIO.OUT)
+GPIO.setup(PIN_GREED,GPIO.OUT)
+
+while 1:
+   
+    logging.debug("LED Testing")
+
+    GPIO.output(PIN_BLUE1,  True)
+    GPIO.output(PIN_BLUE2,  True)
+    GPIO.output(PIN_BLUE3,  True)
+    GPIO.output(PIN_BLUE4,  True)
+    GPIO.output(PIN_RED,    True)
+    GPIO.output(PIN_YELLOW, True)
+    GPIO.output(PIN_GREED,  True)
+    
+    time.sleep(1)
+
+    GPIO.output(PIN_BLUE1,  False)
+    GPIO.output(PIN_BLUE2,  False)
+    GPIO.output(PIN_BLUE3,  False)
+    GPIO.output(PIN_BLUE4,  False)
+    GPIO.output(PIN_RED,    False)
+    GPIO.output(PIN_YELLOW, False)
+    GPIO.output(PIN_GREED,  False)
+    
+    time.sleep(1)
+    
+    #GPIO.cleanup()
