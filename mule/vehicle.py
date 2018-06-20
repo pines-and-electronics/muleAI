71fbb75f3b46f2d4cacad8b4942d7854d86f0f94
MarcusJones <jones.0bj3@gmail.com>
2018-06-20 09:06:16 +0200

    Add yaml configuration
    This is a much cleaner configuration method
    
    * from_config classmethod in vehicle
    
    * simplification of mule.py
    
    * configuration files placed in configurations directory

--- mule/vehicle.py
+++ mule/vehicle.py
@@ -1,6 +1,8 @@
 import time
+
 from itertools import count
 from utilities.generic import regulate
+import logging
 
 class Vehicle():
     ''' Vehicle control
@@ -51,18 +53,42 @@ class Vehicle():
             raise KeyError(msg.format(part.__class__.__name__))
 
         self.parts.append(part)
-        
+
         logging.info("Registering {} key(s)".format(len(part.output_keys)))
-        
+
         self.state_keys = self.state_keys.union(set(part.output_keys))
 
-        
+
+    @classmethod
+    def from_config(cls, config):
+        ''' Creates vehicle from parsed configuration
+
+            Argument
+
+            config: list(utilities.ProtoParts)
+                ProtoPart is a POD class; it is expected to
+                have members:
+                    type : class type for vehicle part
+                    arguments: dict of kwargs to be passed to initializer
+        '''
+        logging.info('Loading parts from parsed config')
+
+        vehicle = cls()
+
+        for part in config:
+            vehicle.add(part.type(**part.arguments))
+
+        return vehicle
+
+
+
+
     def start(self):
         ''' Starts vehicle by starting it constituent parts '''
         self.state = dict.fromkeys(self.state_keys, None)
 
-        
-        for i,part in enumerate(self.parts):
+
+        for i, part in enumerate(self.parts):
             logging.info("Starting part {} of {}, {}".format(i+1,len(self.parts),part))
             part.start()
 
@@ -71,23 +97,22 @@ class Vehicle():
     # TODO: log moving average of loop times
     # TODO: implement maximum number of drive loops ???  
     #       I fail to see the usefulness at the moment
-    # TODO: MJ - Can we move the Regulator to self? 
-    def drive(self, rps=10):
+    def drive(self, freq_hertz=10):
         ''' Engages drive loop
 
             Arguments
 
-            rps: int
+            freq_hertz: int
                 number of drive loops to complete per second
 
             Iterates through parts, each transforming the state. Contains
-            regulator that ensures rps.
+            regulator that ensures freq_hertz.
         '''
 
-        logging.info("Starting drive loop at {} Hz".format(rps))
+        logging.info("Starting drive loop at {} Hz".format(freq_hertz))
 
         try:
-            for loop_nr in regulate(count(), rps):
+            for loop_nr in regulate(count(), freq_hertz):
 
                 for part in self.parts:
                     part.transform(self.state)
