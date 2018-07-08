f82f14a9cd9eeca26c644e8ecd8ed39fa915f6e1
MarcusJones <jones.0bj3@gmail.com>
2018-07-08 13:04:57 +0200

    update

--- mule/parts/led.py
+++ mule/parts/led.py
@@ -5,15 +5,10 @@ import time
 import RPi.GPIO as GPIO
 
 class sequential_LED_loop(BasePart):
-    ''' asdf '''
+    """ """
     input_keys = ()
     output_keys = ('led_flags',)
-    #output_keys = ()
-    #last_flags = state['led_flags']
-    #print(last_flags)
     
-    
-
     def __init__(self,PIN_BLUE1,PIN_BLUE2,PIN_BLUE3, PIN_BLUE4  ):
         NUMBER_LED = 4
         self.lights_off = [False for i in range(NUMBER_LED)]
@@ -27,7 +22,7 @@ class sequential_LED_loop(BasePart):
         self.count = 0
         
     def start(self):
-        ''' asdf '''
+        """ """
         #state['led_flags'] = self.lights_on
         GPIO.setwarnings(False)
         
@@ -41,12 +36,10 @@ class sequential_LED_loop(BasePart):
         GPIO.setup(self.PIN_BLUE4,GPIO.OUT)
 
     def transform(self, state):
-        ''' asdf
-        '''
-        #coin_flips = [random.random() < 0.5 for i in range(5)]
-        #print(coin_flips)
-        #state['led_flags'] = coin_flips
-        state['led_flags'] = self.lights_off
+        """ """
+        #state['led_flags'] = self.lights_off
+        
+        state['led_flags'] = [False for i in range(NUMBER_LED)]
         print("Falses:", self.lights_off)
         print("Before: ",state['led_flags'])
  
@@ -55,24 +48,12 @@ class sequential_LED_loop(BasePart):
             state['led_flags'][self.count%5-1] = True
             
         print("After:",state['led_flags'])
-        #print(self.count, self.count%5)
         self.count += 1
         
         GPIO.output(self.PIN_BLUE1,  state['led_flags'][0])
         GPIO.output(self.PIN_BLUE2,  state['led_flags'][1])
         GPIO.output(self.PIN_BLUE3,  state['led_flags'][2])
         GPIO.output(self.PIN_BLUE4,  state['led_flags'][3])
-        
-     
-        
-        #print(self.count,self.count%5)
-        
-        
-        
-        #print(bool(random.getrandbits(5)))
-        #print(random.sample([True, False],5))
-        #if coin_flip: state['led_flags'] = self.lights_on
-        #else:  state['led_flags'] = self.lights_off
 
     def stop(self):
         '''  '''
