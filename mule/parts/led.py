182257da80e2d5f4f097fbda9110ae5f31a4c8f4
MarcusJones <jones.0bj3@gmail.com>
2018-07-08 09:30:03 +0200

    add Template for LED part

---
+++ mule/parts/led.py
@@ -0,0 +1,24 @@
+from parts.base import BasePart
+
+
+class LED_loop(BasePart):
+    ''' asdf '''
+    input_keys = ()
+    output_keys = ()
+
+    def __init__(self, window_name):
+        pass
+
+    def start(self):
+        ''' asdf '''
+        pass
+
+    def transform(self, state):
+        ''' asdf
+        '''
+        pass
+
+
+    def stop(self):
+        ''' Closes window '''
+        pass
