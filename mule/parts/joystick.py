a6c04207178f22768d3c0abfd17d7b86c41de329
MarcusJones <jones.0bj3@gmail.com>
2018-07-10 19:21:43 +0200

    fix format

--- mule/parts/joystick.py
+++ mule/parts/joystick.py
@@ -363,19 +363,19 @@ class PS3Controller(BasePart):
 
         elif tag == 'button-dpad-up' and value == 1:
             self.throttle_scale = min(1.0, self.throttle_scale + THROTTLE_SCALE_SHIFT)
-            logging.debug("{} throttle_scale = {.2f}".format(tag,self.throttle_scale))
+            logging.debug("{} throttle_scale = {:.2f}".format(tag,self.throttle_scale))
 
         elif tag == 'button-dpad-down' and value == 1:
             self.throttle_scale = max(0.0, self.throttle_scale - THROTTLE_SCALE_SHIFT)
-            logging.debug("{} throttle_scale = {.2f}".format(tag,self.throttle_scale))
+            logging.debug("{} throttle_scale = {:.2f}".format(tag,self.throttle_scale))
 
         elif tag == 'button-dpad-right' and value == 1:
             self.steering_scale = min(1.0, self.steering_scale + STEERING_SCALE_SHIFT)
-            logging.debug("{} steering_scale = {.2f}".format(tag,self.steering_scale))
+            logging.debug("{} steering_scale = {:.2f}".format(tag,self.steering_scale))
 
         elif tag == 'button-dpad-left' and value == 1:
             self.steering_scale = max(0.0, self.steering_scale - STEERING_SCALE_SHIFT)
-            logging.debug("{} steering_scale = {.2f}".format(tag,self.steering_scale))
+            logging.debug("{} steering_scale = {:.2f}".format(tag,self.steering_scale))
 
         elif tag == 'button-triangle' and value == 1:
             self.mode['steering'] = 'human'
