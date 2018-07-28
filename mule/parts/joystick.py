4a9a7bbdadec0dbf4db4b9bcd8ac5416c8cc23e2
MarcusJones <jones.0bj3@gmail.com>
2018-07-28 14:57:45 +0000

    update Replace the mapping dictionary with donkey car names

--- mule/parts/joystick.py
+++ mule/parts/joystick.py
@@ -113,7 +113,7 @@ _AXIS_NAME_LOOKUP = {
 
 
 
-_BUTTON_NAME_LOOKUP = {
+_BUTTON_NAME_LOOKUP_OLD = {
         0x120: 'button-select',         # 288   | 'trigger'
         0x121: 'button-thumb-left',     # 289   | 'thumb'
         0x122: 'button-thumb-right',    # 290   | 'thumb2'
@@ -135,6 +135,54 @@ _BUTTON_NAME_LOOKUP = {
         0x2c2: 'button-?-2'             # 706   | (null)
         }
 
+
+_BUTTON_NAME_LOOKUP = {
+            0x120 : 'trigger',
+            0x121 : 'thumb',
+            0x122 : 'thumb2',
+            0x123 : 'top',
+            0x124 : 'top2',
+            0x125 : 'pinkie',
+            0x126 : 'base',
+            0x127 : 'base2',
+            0x128 : 'base3',
+            0x129 : 'base4',
+            0x12a : 'base5',
+            0x12b : 'base6',
+
+            #PS3 sixaxis specific
+            0x12c : "triangle",
+            0x12d : "circle",
+            0x12e : "cross",
+            0x12f : 'square',
+
+            0x130 : 'a',
+            0x131 : 'b',
+            0x132 : 'c',
+            0x133 : 'x',
+            0x134 : 'y',
+            0x135 : 'z',
+            0x136 : 'tl',
+            0x137 : 'tr',
+            0x138 : 'tl2',
+            0x139 : 'tr2',
+            0x13a : 'select',
+            0x13b : 'start',
+            0x13c : 'mode',
+            0x13d : 'thumbl',
+            0x13e : 'thumbr',
+
+            0x220 : 'dpad_up',
+            0x221 : 'dpad_down',
+            0x222 : 'dpad_left',
+            0x223 : 'dpad_right',
+
+            # XBox 360 controller uses these codes.
+            0x2c0 : 'dpad_left',
+            0x2c1 : 'dpad_right',
+            0x2c2 : 'dpad_up',
+            0x2c3 : 'dpad_down',
+        }
 
 
 class JoystickDevice:
