d101d850dbb043d8bbf651c894eec276e163db44
MarcusJones <jones.0bj3@gmail.com>
2018-07-29 19:25:16 +0200

    add Files loaded into memory

--- mule/offline_train.py
+++ mule/offline_train.py
@@ -40,42 +40,84 @@ assert os.path.exists(LOCAL_PROJECT_PATH)
 directoy_list = glob.glob(os.path.join(LOCAL_PROJECT_PATH,'*'))
 
 # Iterate over each directory
-dataset_dirs = dict()
+dataset_def_list = list()
 for i,this_dir in enumerate(directoy_list):
-    this_dt_string = os.path.split(this_dir)[1]
-    this_dt = datetime.datetime.strptime(this_dt_string, '%Y%m%d %H%M%S')
-    this_dt_iso = this_dt.isoformat()
+    dataset_def = dict()
+    # Time stamp
+    dataset_def['this_dt_string'] = os.path.split(this_dir)[1]
+    dataset_def['this_dt'] = datetime.datetime.strptime(dataset_def['this_dt_string'], '%Y%m%d %H%M%S')
+    dataset_def['this_dt_iso'] = dataset_def['this_dt'].isoformat()
+    dataset_def['this_dt_nice'] = dataset_def['this_dt'].strftime("%A %d %H:%M")
     
-    #print("")
-
-res = [(this_dir,os.path.split(this_dir)[1]) for this_dir in directoy_list]
-this = res[-1][1]
-
-
+    # Get the state zip
+    this_zip = os.path.join(this_dir,'state.zip')
+    assert os.path.exists(this_zip)
+    dataset_def['zip'] = os.path.join(this_dir,'state.zip')
+    dataset_def['size'] = os.path.getsize(this_zip)/1000/1000
+    
+    # Look inside
+    with zipfile.ZipFile(this_zip, "r") as f:
+        #print(f)
+        npy_files = [name for name in f.namelist() if os.path.splitext(name)[1] =='.npy']
+        json_files = [name for name in f.namelist() if os.path.splitext(name)[1] =='.json']
+        other_files = [name for name in f.namelist()]
+        assert len(npy_files) + len(json_files) + 1 == len(other_files)
+        #print(len(other_files))
+        
+    dataset_def['num_records'] = len(npy_files)
+    #print(dataset_def)
+    dataset_def_list.append(dataset_def)
+    #dataset_def_dict
+df = pd.DataFrame(dataset_def_list)
+#df.columns
 
 #%%
-for entry in directoy_list:
-    if os.path.isdir(entry):
-        logging.debug("Directory {}".format(entry))
-        this_zip = os.path.join(entry,'state.zip')
-        assert os.path.exists(this_zip)
+row_str = "{:<4} {:<20} {:<12} {:<6.0f}"
+head_str = "{:<4} {:<20} {:<12} {:<6}"
+
+fields = ('this_dt_nice','num_records','size')
+head = ['idx'] + [*fields]
+print(head_str.format(*head))
+for i,r in df.iterrows():
+    this_row_str = [r[fld] for fld in fields]
+    print(row_str.format(i,*this_row_str))
+
+ds_idx = int(input("Select dataset number:") )
+this_dataset = df.iloc[ds_idx]
+
+#%%
+npy_records = list()
+json_records = list()
+with zipfile.ZipFile(this_dataset['zip'], "r") as f:
+    
+    npy_file_paths = [name for name in f.namelist() if os.path.splitext(name)[1] =='.npy']
+    json_file_paths = [name for name in f.namelist() if os.path.splitext(name)[1] =='.json']
+    
+    for npy_file in npy_file_paths:
+        data = f.read(npy_file)
+        npy_records.append(np.frombuffer(data))
+    for json_file in json_file_paths:
+        d = f.read(json_file)
+        d = json.loads(d.decode("utf-8"))
+        json_records.append(d)
+
+
+logging.debug("{} json and {} npy records loads".format(len(json_records),len(npy_records)))
+
+#print(f)
+
+#%%
+
+        # Look inside
         with zipfile.ZipFile(this_zip, "r") as f:
+            #print(f)
             npy_files = [name for name in f.namelist() if os.path.splitext(name)[1] =='.npy']
             json_files = [name for name in f.namelist() if os.path.splitext(name)[1] =='.json']
             other_files = [name for name in f.namelist()]
             assert len(npy_files) + len(json_files) + 1 == len(other_files)
-            
-            for 
-            
-            #for name in f.namelist():
-            #    data = f.read(name)
-            #    print(name)
-        #print name, len(data), repr(data[:10])
-        
-    #    this_zip
-        #print(entry)    
-    else:
-        raise
+            #print(len(other_files))
+                        
+
 #%%
 
 X_keys = ['cam/image_array']
